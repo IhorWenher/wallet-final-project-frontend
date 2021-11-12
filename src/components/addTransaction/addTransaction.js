ea80a35a1dd0b56b71747e4bf095840bb96ca6da
Maxim Karimov <Juk3z1973@gmail.com>
2021-11-12 05:54:00 +0500

    Добавил базовую логику

--- src/components/addTransaction/addTransaction.js
+++ src/components/addTransaction/addTransaction.js
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 // import Datetime from 'react-datetime';
 // import { Link, useLocation } from 'react-router-dom';
 // import fetch from '../../service';
@@ -8,22 +8,59 @@ import 'moment/locale/ru'
 import styles from "./styles.module.css";
 
 function AddTransaction() {
+    const [transactionType, setTransactionType] = useState("income");
+    const [category, setCategory] = useState("Выберите категорию");
+    const [listActive, setListActive] = useState(false)
+
+    function radioClickHandler(e) {
+         setTransactionType(e.target.value)
+    }
+
+    function categoryClickHandler(e) {
+        setCategory(e.target.textContent)
+        setListActive(!listActive)
+    }
+
+    function listOpen(e) {
+        setListActive(!listActive)
+    }
+
+    const dropDownJSX = (
+        <div>
+            <div className={styles.dropDownContainer} onClick={listOpen}>
+                <span className={styles.selectedCategory}>{category}(можно нажать)</span>
+            </div>
+
+            {listActive && <ul className={styles.dropDownList}>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Основной</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Еда</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Авто</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Развитие</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Дети</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Дом</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Образование</li>
+                <li onClick={categoryClickHandler} className={styles.dropDownItem}>Остальные</li>
+            </ul>}
+        </div>
+    )
  
 
     return (
         <div className={styles.container}>
             <h2>Добавить транзакцию</h2>
             <form className={styles.typeContainer}>
-                <input name="transactionType" type="radio" id="income" />
+                <input defaultChecked onClick={radioClickHandler} name="transactionType" type="radio" id="income" value="income"/>
                 <label htmlFor="income">Доход</label>
 
-                <input name="transactionType" type="radio" id="spending" />
+                <input onClick={radioClickHandler} name="transactionType" type="radio" id="spending" value="spending"/>
                 <label htmlFor="spending">Расход</label>
+
+                {/* рендер списка по условию */}
+                {transactionType === 'income' && dropDownJSX}
 
                 <input className={styles.formField} type="number" placeholder="0.00"/>
                 
-                {/* <input className={styles.formField} type="date" /> */}
-                <Datetime initialValue={new Date()} closeOnSelect={true} timeFormat={false} />
+                <Datetime initialValue={new Date()} closeOnSelect={true} timeFormat={false}  />
                 
                 <textarea className={styles.formField} type="text" placeholder="ekekeke" />
                 
