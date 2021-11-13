7468e5824028d74fa7a053fcf9225cd322e83cca
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-11-13 10:23:11 +0200

    fix import

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,14 +1,16 @@
 import Styles from './MainView.module.css';
 import Dashboard from '../../components/Dashboard';
 import AddTransactionBtn from '../../components/AddTransactionBtn';
-import AddTransaction from "./components/addTransaction";
+import AddTransaction from '../../components/addTransaction';
 
 const MainView = () => {
-  return <div className={Styles.container}>
-    <Dashboard />
-    <AddTransactionBtn />
-    <AddTransaction />
-  </div>;
+  return (
+    <div className={Styles.container}>
+      <Dashboard />
+      <AddTransactionBtn />
+      <AddTransaction />
+    </div>
+  );
 };
 
 export default MainView;
