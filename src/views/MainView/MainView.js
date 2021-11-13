85f3f005ff035a454e69ffb391c6918bb6947471
Yourlexs <you@example.com>
2021-11-13 19:04:31 +0300

    added open modal

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,14 +1,22 @@
+import { useState } from 'react';
+
 import Styles from './MainView.module.css';
 import Dashboard from '../../components/Dashboard';
 import AddTransactionBtn from '../../components/AddTransactionBtn';
 import AddTransaction from '../../components/addTransaction';
 
 const MainView = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div className={Styles.container}>
       <Dashboard />
-      <AddTransactionBtn />
-      <AddTransaction />
+      <AddTransactionBtn toggleModal={toggleModal} />
+      {showModal && (<AddTransaction />)}
     </div>
   );
 };
