4e439386b7dfb94f5089ae334de897932d01d126
Yourlexs <you@example.com>
2021-11-13 16:55:17 +0300

    redux

---
+++ src/redux/transactions/index.js
@@ -0,0 +1,4 @@
+export *  from './transactions-operations';
+export * from './transactions-selectors';
+export * from './transactions-actions';
+export { default as transactionsReducer } from './transactions-reducer';
