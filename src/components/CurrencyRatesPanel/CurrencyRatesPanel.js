108d629ca24ace6ea70099e0545af824381521b2
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-11-14 21:44:49 +0200

    adds classes and fixes formats

--- src/components/CurrencyRatesPanel/CurrencyRatesPanel.js
+++ src/components/CurrencyRatesPanel/CurrencyRatesPanel.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrencies } from '../../redux/currency/currencyOperations';
 import currencySelector from "../../redux/currencySelectors";
+import Vector from './vector.svg';
+import styles from './CurrencyRatesPanel.module.css';
 
 function CurrencyRatesPanel() {
 
@@ -14,13 +16,26 @@ function CurrencyRatesPanel() {
         dispatch(getCurrencies);
     }, [dispatch]);
 
+    const formatRate = (rate) => rate > 9 ? "" + Number(rate).toFixed(2): "0" + Number(rate).toFixed(2);  
+
     return (
-        <div>
+        <div className={styles.currencyRatesPanel}>
+            <ul className={styles.currencyRatesHead}>
+                <li>Валюта</li>
+                <li>Покупка</li>
+                <li>Продажа</li>
+            </ul>
             {currencyRatesLoading
-             ? <div><h1>Loading rates ...</h1></div>
-             : <div>
-                 {currencyRates.map(({ ccy, buy, sale }) => <h1 key={ccy}>{`${ccy} ${buy} ${sale}`}</h1>)}
-               </div>}
+            ? <div><h1>Loading rates ...</h1></div>
+            : <ul className={styles.currencyRatesList}>
+                    {currencyRates.map(({ ccy, buy, sale }) => 
+                        <li className={styles.currencyRatesListItem} key={ccy}>
+                            <p>{ccy}</p>
+                            <p>{formatRate(buy)}</p>
+                            <p>{formatRate(sale)}</p>
+                        </li>)}
+                </ul>}         
+                <img src={Vector} alt="vector" className={styles.vector}/>
         </div>
     );
 }
