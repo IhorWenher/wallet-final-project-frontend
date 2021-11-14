6f66445d823514304260c85433b2874e02d441b3
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-11-14 21:42:32 +0200

    moved component

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,13 +1,13 @@
 import Styles from './MainView.module.css';
 import Dashboard from '../../components/Dashboard';
 import AddTransactionBtn from '../../components/AddTransactionBtn';
-import CurrencyRatesPanel from '../../components/CurrencyRatesPanel/CurrencyRatesPanel';
+import CurrencyRatesPanel from '../../components/CurrencyRatesPanel';
 
 const MainView = () => {
   return <div className={Styles.container}>
+    <CurrencyRatesPanel />
     <Dashboard />
     <AddTransactionBtn/>
-    <CurrencyRatesPanel />
   </div>;
 };
 
