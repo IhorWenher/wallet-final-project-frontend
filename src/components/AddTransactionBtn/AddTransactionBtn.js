9e1703c54d5af426ad0641a83a475346dd188522
Yourlexs <you@example.com>
2021-11-14 14:31:01 +0300

    added svg

--- src/components/AddTransactionBtn/AddTransactionBtn.js
+++ src/components/AddTransactionBtn/AddTransactionBtn.js
@@ -1,11 +1,11 @@
+import { ReactSVG } from 'react-svg'
+import svgPlus from '../../images/plus-icon.svg'
 import styles from './AddTransactionBtn.module.css';
 
 export default function OpenTransactionBtn({toggleModal}) {
   return (
     <button type="button" className={styles.btn} onClick={toggleModal}>
-      <svg>
-        <use xlinkHref='../../images/plus-icon.svg'></use>
-      </svg>
+      <ReactSVG src={svgPlus}/>
     </button>
   );
 }
--- src/redux/transactions/index.js
+++ src/redux/transactions/index.js
@@ -1,4 +1,4 @@
-export *  from './transactions-operations';
-export * from './transactions-selectors';
+export * from './transactions-operations';
+export { getTransactions } from './transactions-selectors';
 export * from './transactions-actions';
 export { default as transactionsReducer } from './transactions-reducer';
