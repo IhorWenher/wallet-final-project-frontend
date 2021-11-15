64fa460365f48b0be9e62efae4afb568fbcfae8e
Iryna Volosozharova <iradevis@gmail.com>
2021-11-15 23:29:26 +0200

    login markup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Container from './components/Container';
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+const RegisterView = lazy(() => import('./views/RegisterView/RegisterView.js'));
+const LoginView = lazy(() => import('./views/LoginView/LoginView.js'));
 const MainView = lazy(() => import('./views/MainView/MainView.js'));
 
 function App() {
@@ -9,6 +11,21 @@ function App() {
     <Container>
       <Suspense fallback={<h2>Loading...</h2>}>
         <Routes>
+          <Route
+            path="/login"
+            // redirectTo="/"
+            element={<LoginView />}
+            restricted
+          ></Route>
+
+          <Route
+            exact
+            path="/register"
+            // redirectTo="/"
+            element={<RegisterView />}
+            restricted
+          ></Route>
+
           <Route path="/" exact element={<MainView />}></Route>
         </Routes>
       </Suspense>
