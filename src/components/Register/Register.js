097fd7f367d2ab6419e62ce5cb45278c28ffb0bb
Iryna Volosozharova <iradevis@gmail.com>
2021-11-18 00:36:01 +0200

    login/register base operations

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,37 @@
 import Styles from './Register.module.css';
 import React from 'react';
 
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from '../../redux/auth';
+
 export default function Register() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <div className={Styles.container}>
       <div className={Styles.heroContainer}>
@@ -34,9 +64,15 @@ export default function Register() {
             </svg>
           </div>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>
-              <input className={Styles.input} placeholder="E-mail"></input>
+              <input
+                className={Styles.input}
+                placeholder="E-mail"
+                onChange={handleChange}
+                name="email"
+                value={email}
+              ></input>
               <svg width="21" height="16" className={Styles.inputIcon}>
                 <path
                   d="M18 0H2C.9 0 .00999999.9.00999999 2L0 14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2Zm0 4-8 5-8-5V2l8 5 8-5v2Z"
@@ -46,7 +82,13 @@ export default function Register() {
             </label>
 
             <label>
-              <input className={Styles.input} placeholder="Пароль"></input>
+              <input
+                className={Styles.input}
+                placeholder="Пароль"
+                onChange={handleChange}
+                name="password"
+                value={password}
+              ></input>
               <svg width="16" height="21" className={Styles.inputIcon}>
                 <path
                   d="M14 7h-1V5c0-2.76-2.24-5-5-5S3 2.24 3 5v2H2C.9 7 0 7.9 0 9v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2Zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2Zm3.1-9H4.9V5c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2Z"
@@ -69,7 +111,13 @@ export default function Register() {
             </label>
 
             <label>
-              <input className={Styles.input} placeholder="Ваше имя"></input>
+              <input
+                className={Styles.input}
+                placeholder="Ваше имя"
+                onChange={handleChange}
+                name="name"
+                value={name}
+              ></input>
               <svg width="18" height="18" className={Styles.inputIcon}>
                 <path
                   d="M0 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2H2C.89 0 0 .9 0 2Zm12 4c0 1.66-1.34 3-3 3S6 7.66 6 6s1.34-3 3-3 3 1.34 3 3Zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H3v-1Z"
@@ -79,7 +127,9 @@ export default function Register() {
             </label>
 
             <button className={Styles.logBtn}>вход</button>
-            <button className={Styles.regBtn}>регистрация</button>
+            <button type="submit" className={Styles.regBtn}>
+              регистрация
+            </button>
           </form>
         </div>
       </div>
