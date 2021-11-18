7c3e2c0522f2d9fc7e8e3bc6bcf18efca2a07f25
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-11-18 23:55:58 +0200

    fixed_buttons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import Styles from './Login.module.css';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
@@ -93,14 +92,12 @@ export default function Login() {
                 />
               </svg>
             </label>
-            <NavLink to="/login" className={Styles.link}>
-              <button className={Styles.logBtn} type="submit">
-                вход
-              </button>
-            </NavLink>
-            <NavLink to="/register" className={Styles.link}>
-              <button className={Styles.regBtn}>регистрация</button>
-            </NavLink>
+
+            <button className={Styles.logBtn} type="submit">
+              вход
+            </button>
+
+            <button className={Styles.regBtn}>регистрация</button>
           </form>
         </div>
       </div>
