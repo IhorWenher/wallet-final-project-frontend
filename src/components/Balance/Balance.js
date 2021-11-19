5e2c334ab725a62c4211a39085ee760919a0777e
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-11-19 18:28:13 +0200

    adds styles for balanc and currency panel

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -4,8 +4,6 @@ import styles from './Balance.module.css';
 import { fetchBalance } from '../../redux/balance/balance-operations';
 import { balance as balanceSelector } from '../../redux/balance/balance-selectors';
 
-const moneyFormat = Intl.NumberFormat('ru-RU');
-
 function Balance() {
     const balance = useSelector(balanceSelector);
     const dispatch = useDispatch();
@@ -14,10 +12,12 @@ function Balance() {
         dispatch(fetchBalance)
     }, [dispatch]);
 
+    const formatBalance = balance => balance.toLocaleString('ru-RU', { minimumFractionDigits: 2 }).replace(',', '.');
+
     return (
         <div className={styles.balance}>
             <h2 className={styles.balanceTitle}>Ваш баланс</h2>
-            <h2 className={styles.balanceValue}> &#8372; {moneyFormat.format(balance)}</h2>
+            <h2 className={styles.balanceValue}> <span className={styles.currencySymbol}>&#8372;</span> {balance ? formatBalance(balance) : ''}</h2>
         </div>
     );
 }
