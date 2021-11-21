d2a86581a6e8831e9af2a147416183aa9cf0c5c0
Maxim Karimov <Juk3z1973@gmail.com>
2021-11-21 23:28:58 +0500

    немного причесал код

--- src/api/balanceApi.js
+++ src/api/balanceApi.js
@@ -4,7 +4,6 @@ axios.defaults.baseURL = 'https://final-project-back.herokuapp.com/api';
 
 async function getBalance() {
   const { data } = await axios.get('/users/balance');
-  console.log(data);
   return data.data.user.balance;
 }
 
